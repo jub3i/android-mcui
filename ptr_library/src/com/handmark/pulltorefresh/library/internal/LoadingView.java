e417d89461ed7ebb937a3fb196adb4a441a6b8cd
jub3i <anon66@gmail.com>
2012-07-12 15:59:50 +0200

    made animation and loading layout density independent

--- ptr_library/src/com/handmark/pulltorefresh/library/internal/LoadingView.java
+++ ptr_library/src/com/handmark/pulltorefresh/library/internal/LoadingView.java
@@ -1,7 +1,5 @@
 package com.handmark.pulltorefresh.library.internal;
 
-import com.handmark.pulltorefresh.library.R;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,182 +7,225 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.view.View;
+import com.handmark.pulltorefresh.library.R;
 
 public class LoadingView extends View {
 	//enum for the various animations the LoadingView can do.
-	public enum AnimMode {
+	private enum AnimMode {
         A_UP , A_DOWN, A_DOWN_UP, A_DRAW_FULL
     }
 	
+	//Scale used on different density screens, to calculate animation movements.
+	private float mScale = getContext().getResources().getDisplayMetrics().density;
+	//times used to control fps of animation.
+	private long mTimeFrameLastDrawnAt = 0;
+	private long mCurrentTime;
+	
+	//enum for which mode of animation the LoadingView is in.
+    private AnimMode mMode;
+    //boolean controlling the cycling between up and down animations.
+	private boolean mIsAnimUp;
+	
 	//x,y coordinates for the animations start and end position.
 	//x,y directions indicated in diagram, ie. y-axis is inverted.
-    //             (-y)
-	//  	        |
-	//  	(-x) ---+--- (+x)
-	//  	  	    |
-	//		 	   (+y)
-	private final int X_START_POS = -48; //start as far to the left as possible.
-    private final int Y_START_POS = -44; 
-    private final int Y_END_POS = 14; //how far down the beer level drops.
+	//            (-y)
+	//              |
+	//      (-x) ---+--- (+x)
+	//              |
+	//            (+y)
+	private final int X_START_POS = (int) (-128*mScale + 0.5f);
+	private final int Y_START_POS = (int) (-27*mScale + 0.5f);
+    private final int Y_END_POS = (int) (20*mScale + 0.5f);
+    
+    //position to draw the animation frames when done.
+    private final int mScaledX = (int) (-42*mScale + 0.5f);
+    private final int mScaledY = (int) (-37*mScale + 0.5f);
+    
+    //movement in x,y of the beer level, for different screen densities.
+    private float mDeltaX = (2*mScale);
+    private float mDeltaY = (1*mScale);
     
     //since the pulltorefresh_library uses 4 instances of LoadingLayout, 
-    //we need to sync the positions of the animations to make movements 
+    //we need to sync the position of the beer level to make movements 
     //up and down appear smooth.
-    static int currentX = -48;
-    static int currentY = -44;
-    
-    //enum for which mode of animation the LoadingView is in.
-    private AnimMode mode;
-    //boolean controlling the cycling between up and down animations.
-	private boolean isAnimUp;
+    private static float mCurrentX;
+    private static float mCurrentY;
     
 	//Instantiate static resources used in the animation.
-    Bitmap mainImage = BitmapFactory.decodeResource(getResources(),R.drawable.background);
-    Bitmap mask = BitmapFactory.decodeResource(getResources(), R.drawable.backmask);
-    Bitmap foreground = BitmapFactory.decodeResource(getResources(), R.drawable.foreground);
+    private Bitmap mBackground = BitmapFactory.decodeResource(getResources(),
+    		R.drawable.background);
+    private Bitmap mBackgroundMask = BitmapFactory.decodeResource(getResources(), 
+    		R.drawable.backmask);
+    private Bitmap mForeground = BitmapFactory.decodeResource(getResources(), 
+    		R.drawable.foreground);
     //create resulting bitmap used to draw the LoadingView.
-    Bitmap result = Bitmap.createBitmap(mainImage.getWidth(), mainImage.getHeight(), Bitmap.Config.ARGB_8888);
+    private Bitmap mResult = Bitmap.createBitmap(mBackground.getWidth(), 
+    		mBackground.getHeight(), Bitmap.Config.ARGB_8888);
+    
     //drawing resources.
-    Paint paint = new Paint();
-    Canvas c = new Canvas(result);
-
+    private Paint mPaint = new Paint();
+    private Canvas mResultCanvas = new Canvas(mResult);
+    
     //constructor
     public LoadingView(Context context) {
 		super(context);
 		init();
-	}
-    
+    }
+
     //constructor
     public LoadingView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+    	super(context, attrs);
         init();
 	}
-	
+
     //helper function for constructors.
 	private void init() {
-		paint.setFilterBitmap(false);
-		mode = AnimMode.A_DRAW_FULL;
-		isAnimUp = true;
+		mPaint.setFilterBitmap(false);
+		mMode = AnimMode.A_DRAW_FULL;
+		mIsAnimUp = true;
+		mCurrentX = X_START_POS;
+		mCurrentY = Y_START_POS;
 	}
-	
+
 	//public function to call the animating of the LoadingView.
 	//animates the beer level dropping.
 	public void animDown() {
-		mode = AnimMode.A_DOWN;
+		mMode = AnimMode.A_DOWN;
 		invalidate();
 	}
 	//public function to call the animating of the LoadingView.
 	//animates the beer level rising.
 	public void animUp() {
-		mode = AnimMode.A_UP;
+		mMode = AnimMode.A_UP;
 		invalidate();
 	}
 	//public function to call the animating of the LoadingView.
 	//animates the beer level cycling between falling first then rising.
 	public void animDownUp() {
-		mode = AnimMode.A_DOWN_UP;
+		mMode = AnimMode.A_DOWN_UP;
 		invalidate();
 	}
 	//public function to call the animating of the LoadingView.
 	//used to reset the LoadingView.
 	public void animDrawFull() {
-		currentX = X_START_POS;
-		currentY = Y_START_POS;
-		mode = AnimMode.A_DRAW_FULL;
+		mCurrentX = X_START_POS;
+		mCurrentY = Y_START_POS;
+		mMode = AnimMode.A_DRAW_FULL;
 		invalidate();		
 	}
 	
 	//helper function for the onDraw() method.
 	//draws the static resources onto the canvas used to draw the LoadingView.
-	private void drawHelper(int x, int y, Canvas c) {
-		c.drawBitmap(mask, 0, 0, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        c.drawBitmap(mainImage, x, y, paint);
-        paint.setXfermode(null);
-        c.drawBitmap(foreground, 0, 0, paint);
+	private void drawFrame(int x, int y, Canvas c) {
+		c.drawBitmap(mBackgroundMask, 0, 0, mPaint);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        c.drawBitmap(mBackground, x, y, mPaint);
+        mPaint.setXfermode(null);
+        c.drawBitmap(mForeground, 0, 0, mPaint);
 	}
 	
 	@Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        //switch to control which animation is being played.
-        switch (mode) {
-        	//draws the glass full.
-        	case A_DRAW_FULL:
-        		drawHelper(currentX, currentY, c);
-                canvas.drawBitmap(result, -64, -56, null);
-                invalidate();
-                break;
-            //animates the beer level dropping.
-        	case A_DOWN:
-        		if (currentY <= Y_END_POS) {
-        			currentX += 2;
-	            	currentY++;
-        		}
-        		drawHelper(currentX, currentY, c);
-	            canvas.drawBitmap(result, -64, -56, null);
-        		
-	            if (currentY <= Y_END_POS)
+		super.onDraw(canvas);
+		mCurrentTime = SystemClock.uptimeMillis();
+		
+		//time controlled frame rate: +25ms => 40fps
+        if ((mTimeFrameLastDrawnAt + 25) < mCurrentTime) {
+        	//switch to control which animation is being played.
+			switch (mMode) {
+	        	//draws the glass full.
+	        	case A_DRAW_FULL:
+	        		drawFrame((int) mCurrentX, (int) mCurrentY, mResultCanvas);
+	        		canvas.drawBitmap(mResult, mScaledX, mScaledY, null);
+	        		mTimeFrameLastDrawnAt = SystemClock.uptimeMillis();
+	        		invalidate();
+	                break;
+	            
+	            //animates the beer level dropping.
+	        	case A_DOWN:
+	        		if (mCurrentY <= Y_END_POS) {
+	        			mCurrentX += mDeltaX;
+		            	mCurrentY += mDeltaY;
+	        		}
+	        		drawFrame((int) mCurrentX, (int) mCurrentY, mResultCanvas);
+	        		canvas.drawBitmap(mResult, mScaledX, mScaledY, null);
+	            	mTimeFrameLastDrawnAt = SystemClock.uptimeMillis();
 	            	invalidate();
-	            break;
-	        //animates the beer level rising.	
-        	case A_UP:
-        		if (currentY >= Y_START_POS) {
-        			currentX -= 2;
-        			currentY--;
-        		}
-        		drawHelper(currentX, currentY, c);
-        		canvas.drawBitmap(result, -64, -56, null);
-        		
-	            if (currentY >= Y_START_POS) 
-	               	invalidate();                	
-	            break;
-	        //animates the beer level cycling between rising and falling.	
-        	case A_DOWN_UP:
-        		//start by dropping the beer level to the bottom.
-        		if (isAnimUp == true) {
-        			if (currentY <= Y_END_POS) {
-            			currentX += 2;
-    	            	currentY++;
-        			}
-        			drawHelper(currentX, currentY, c);
-    	            canvas.drawBitmap(result, -64, -56, null);
-        			
-    	            if (currentY <= Y_END_POS)
-    	            	invalidate();
-    	            else {
-    	            	//when beer level dropped to Y_END_POS, toggle beer level rising boolean.
-    	            	isAnimUp = false; 
-    	            	invalidate();
-    	            }
-    	        }
-        		//then rise the beer level, repeat there after.
-        		else {
-        			if (currentY >= Y_START_POS) {
-            			currentX -= 2;
-            			currentY--;
-        			}
-        			drawHelper(currentX, currentY, c);
-            		canvas.drawBitmap(result, -64, -56, null);
-        			
-    	            if (currentY >= Y_START_POS) 
-    	               	invalidate();
-    	            else {
-    	            	//and repeat.
-    	            	isAnimUp = true;
-    	            	invalidate();
-    	            }
-    	        }
-        		break;
+		            break;
+		        
+		        //animates the beer level rising.	
+	        	case A_UP:
+	        		if (mCurrentY >= Y_START_POS) {
+	        			mCurrentX -= mDeltaX;
+	        			mCurrentY -= mDeltaY;
+	        		}
+	        		drawFrame((int) mCurrentX, (int) mCurrentY, mResultCanvas);
+	        		canvas.drawBitmap(mResult, mScaledX, mScaledY, null);
+	        		mTimeFrameLastDrawnAt = SystemClock.uptimeMillis();
+	            	invalidate();                	
+		            break;
+
+		        //animates the beer level cycling between rising and falling.	
+	        	case A_DOWN_UP:
+	        		//start by dropping the beer level to the bottom.
+	        		if (mIsAnimUp == true) {
+	        			if (mCurrentY <= Y_END_POS) {
+	            			mCurrentX += mDeltaX;
+	    	            	mCurrentY += mDeltaY;
+	        			}
+	        			drawFrame((int) mCurrentX, (int) mCurrentY, mResultCanvas);
+	        			canvas.drawBitmap(mResult, mScaledX, mScaledY, null);
+	        			
+	        			if (mCurrentY <= Y_END_POS) {
+	    	            	mTimeFrameLastDrawnAt = SystemClock.uptimeMillis();
+	    	            	invalidate();
+	        			}
+	    	            else {
+	    	            	//when beer level dropped past Y_END_POS, 
+	    	            	//toggle beer level rising boolean.
+	    	            	mIsAnimUp = false;
+	    	            	invalidate();
+	    	            }
+	    	        }
+	        		//then rise the beer level, repeat there after.
+	        		else {
+	        			if (mCurrentY >= Y_START_POS) {
+	            			mCurrentX -= mDeltaX;
+	            			mCurrentY -= mDeltaY;
+	        			}
+	        			drawFrame((int) mCurrentX, (int) mCurrentY, mResultCanvas);
+	        			canvas.drawBitmap(mResult, mScaledX, mScaledY, null);
+	        				        			
+	    	            if (mCurrentY >= Y_START_POS) {
+	    	            	mTimeFrameLastDrawnAt = SystemClock.uptimeMillis();
+	    	            	invalidate();
+	    	            }
+	    	            else {
+	    	            	//and repeat.
+	    	            	mIsAnimUp = true;
+	    	            	mTimeFrameLastDrawnAt = SystemClock.uptimeMillis();
+	    	            	invalidate();
+	    	            }
+	    	        }
+	        		break;
+	        }
+        }
+        //else draw again without moving beer level and invalidate.
+        else {
+        	drawFrame((int) mCurrentX, (int) mCurrentY, mResultCanvas);
+        	canvas.drawBitmap(mResult, mScaledX, mScaledY, null);
+        	invalidate();
         }
     }
 	
-	//Sets the size of the view to a static 56x75 pixels.
+	//Sets the size of the view to 45x58 pixels @ 160dpi, scaled for other densities.
 	@Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		setMeasuredDimension(56, 75);
+		int height = (int) (55*(mScale) + 0.5f);
+        int width = (int) (42*(mScale) + 0.5f);
+        setMeasuredDimension(width, height);
     }
 }
